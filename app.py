07a4822d7654734c9b0a4deaec834db82eee9022
Jop <j.bertrams@home.nl>
2023-06-07 15:59:52 +0200

    Fixed MySQL connection and endpoint added
    - MySQL timed out after running application for a while because connection got never closed, now the connection is opened and closed on every request.
    
    - Endpoint added to get loadshedding schedule of the Tshwane campus

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from os.path import join, dirname, realpath
 from flask import *
@@ -19,12 +20,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'mysecretkey'
 CORS(app)
 
-# Create MySQL connection
-db = mysql.connector.connect(
-    host=os.environ['DBHOST'], user=os.environ['DBUSER'], passwd=os.environ['DBPASS'], db=os.environ['DBNAME'], port=os.environ['DBPORT'])
-cursor = db.cursor(dictionary=True)
-db.autocommit = True
-
 
 @app.route('/')
 def home():
@@ -33,22 +28,31 @@ def home():
 
 @app.route('/students')
 def get_students():
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM students;")
     students = cursor.fetchall()
+    cursor.close()
     return json.dumps(students, default=str)
 
 
 @app.route('/students/<id>')
 def get_student(id):
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM students WHERE id = %s;", (id,))
     student = cursor.fetchone()
+    cursor.close()
     return json.dumps(student, default=str)
 
 
 @app.route('/students/<id>/loadsheddinggroup')
 def get_student_loadshedding_group(id):
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM students WHERE id = %s;", (id,))
     student = cursor.fetchone()
+    cursor.close()
     if student['loadsheddingGroup'] == None:
         return json.dumps(get_loadshedding_group(student), default=str)
     return json.dumps(student['loadsheddingGroup'], default=str)
@@ -56,8 +60,11 @@ def get_student_loadshedding_group(id):
 
 @app.route('/students/<id>/loadsheddingschedule')
 def get_student_loadshedding_schedule(id):
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM students WHERE id = %s;", (id,))
     student = cursor.fetchone()
+    cursor.close()
     if student['loadsheddingGroup'] == None:
         return json.dumps('Student has no loadshedding group', default=str)
     # Get loadshedding schedule from Eskom API
@@ -70,22 +77,31 @@ def get_student_loadshedding_schedule(id):
 
 @app.route('/classes')
 def get_classes():
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM classes;")
     classes = cursor.fetchall()
+    cursor.close()
     return json.dumps(classes, default=str)
 
 
 @app.route('/classes/<id>')
 def get_class(id):
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM classes WHERE id = %s;", (id,))
     class_ = cursor.fetchone()
+    cursor.close()
     return json.dumps(class_, default=str)
 
 
 @app.route('/classes/<id>/students')
 def get_class_students(id):
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT students.* FROM students JOIN student_classes ON students.id = student_classes.student_id JOIN classes ON classes.id = student_classes.class_id WHERE classes.id = %s;", (id,))
     students = cursor.fetchall()
+    cursor.close()
     return json.dumps(students, default=str)
 
 
@@ -122,7 +138,60 @@ def upload_file():
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
+
+@app.route('/loadshedding/tshwane_campus')
+def get_tshwane_campus_loadshedding_schedule():
+    # # Get loadshedding schedule from Eskom API
+    # api_url = 'https://developer.sepush.co.za/business/2.0/area?id=tshwane-10-ninapark'
+    # response = requests.get(
+    #     api_url, headers={'token': os.environ['SEPUSHTOKEN']})
+    # loadsheddingToday = []
+    # loadsheddingTomorrow = []
+    # for event in response.json()['events']:
+    #     # Check if the date of event.start is today
+    #     if datetime.datetime.strptime(event['start'], '%Y-%m-%dT%H:%M:%S%z').date() == datetime.date.today():
+    #         loadsheddingToday.append(event)
+    #     # Check if the date of event.start is tomorrow
+    #     if datetime.datetime.strptime(event['start'], '%Y-%m-%dT%H:%M:%S%z').date() == datetime.date.today() + datetime.timedelta(days=1):
+    #         loadsheddingTomorrow.append(event)
+    # return json.dumps({'today': loadsheddingToday, 'tomorrow': loadsheddingTomorrow}, default=str)
+    return json.dumps({
+        "today": [
+            {
+                "end": "2023-06-06T18:30:00+02:00",
+                "note": "Stage 4",
+                "start": "2023-06-06T16:00:00+02:00"
+            }
+        ],
+        "tomorrow": [
+            {
+                "end": "2023-06-07T02:30:00+02:00",
+                "note": "Stage 4",
+                "start": "2023-06-07T00:00:00+02:00"
+            },
+            {
+                "end": "2023-06-07T18:30:00+02:00",
+                "note": "Stage 4",
+                "start": "2023-06-07T16:00:00+02:00"
+            }
+        ]
+    })
+
 # Functions
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host=os.environ['DBHOST'], user=os.environ['DBUSER'], passwd=os.environ['DBPASS'], db=os.environ['DBNAME'], port=os.environ['DBPORT'])
+        g.db.autocommit = True
+    return g.db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if 'db' in g:
+        g.db.close()
 
 
 def read_pdf(filename):
@@ -139,6 +208,8 @@ def allowed_file(filename):
 def get_loadshedding_group(student):
     # Check if student has coordinates
     # If not, get coordinates from Nominatim API
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     coordinates = ''
     if student['coordinates_lat'] == None or student['coordinates_lon'] == None:
         coordinates = get_coordinates(student['address'])
@@ -155,6 +226,7 @@ def get_loadshedding_group(student):
     # Save loadshedding group to database
     cursor.execute("UPDATE students SET loadsheddingGroup = %s WHERE id = %s;",
                    (loadshedding_group, student['id']))
+    cursor.close()
     return loadshedding_group
 
 
