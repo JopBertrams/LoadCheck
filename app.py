bc2f80c80ff0355b62a2cef46c369222d4588865
Jop <j.bertrams@home.nl>
2023-06-19 22:15:55 +0200

    Class info endpoint
    - Endpoint for info about class including:
        - % of students w/ loadshedding
        - amount of students
        - amount of students w/ loadshedding
        - amount of students w/o address
        - students
    - Upload endpoints and page removed
    - API Documentation updated

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
-import datetime
+from datetime import datetime, time, timedelta
 import os
-from os.path import join, dirname, realpath
 from flask import *
 from flask_cors import CORS
-from fileinput import filename
-from werkzeug.utils import secure_filename
 import mysql.connector
 import requests
 import json
-from PyPDF2 import PdfReader
-
-
-UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'uploads')
-ALLOWED_EXTENSIONS = {'pdf'}
 
 # Create Flask app
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'mysecretkey'
 CORS(app)
+
+# Routes
 
 
 @app.route('/')
@@ -36,146 +29,153 @@ def get_students():
     return json.dumps(students, default=str)
 
 
-@app.route('/students/<id>')
-def get_student(id):
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM students WHERE id = %s;", (id,))
-    student = cursor.fetchone()
-    cursor.close()
-    return json.dumps(student, default=str)
-
-
-@app.route('/students/<id>/loadsheddinggroup')
-def get_student_loadshedding_group(id):
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM students WHERE id = %s;", (id,))
-    student = cursor.fetchone()
-    cursor.close()
-    if student['loadsheddingGroup'] == None:
-        return json.dumps(get_loadshedding_group(student), default=str)
-    return json.dumps(student['loadsheddingGroup'], default=str)
+@app.route('/students/<id>', methods=['GET', 'PATCH'])
+def get_update_student(id):
+    if request.method == 'GET':
+        db = get_db()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM students WHERE student_id = %s;", (id,))
+        student = cursor.fetchone()
+        cursor.close()
+        return json.dumps(student, default=str)
+    elif request.method == 'PATCH':
+        db = get_db()
+        cursor = db.cursor(dictionary=True)
+        data = request.get_json()
+        loadsheddingArea = data['loadshedding_area']
+        calendarName = data['calendar_name']
+        cursor.execute("UPDATE students SET loadshedding_area = %s, calendar_name = %s WHERE student_id = %s;",
+                       (loadsheddingArea, calendarName, id,))
+        db.commit()
+        cursor.close()
+        return "Success", 200
 
 
 @app.route('/students/<id>/loadsheddingschedule')
 def get_student_loadshedding_schedule(id):
     db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM students WHERE id = %s;", (id,))
-    student = cursor.fetchone()
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT calendar_name FROM students WHERE student_id = %s;", (id,))
+    calendar_name = cursor.fetchone()[0]
     cursor.close()
-    if student['loadsheddingGroup'] == None:
-        return json.dumps('Student has no loadshedding group', default=str)
-    # Get loadshedding schedule from Eskom API
-    api_url = 'https://developer.sepush.co.za/business/2.0/area?id=' + \
-        student['loadsheddingGroup']
-    response = requests.get(
-        api_url, headers={'token': os.environ['SEPUSHTOKEN']})
-    return json.dumps(response.json(), default=str)
-
-
-@app.route('/classes')
-def get_classes():
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM classes;")
-    classes = cursor.fetchall()
-    cursor.close()
-    return json.dumps(classes, default=str)
-
-
-@app.route('/classes/<id>')
-def get_class(id):
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM classes WHERE id = %s;", (id,))
-    class_ = cursor.fetchone()
-    cursor.close()
-    return json.dumps(class_, default=str)
-
-
-@app.route('/classes/<id>/students')
-def get_class_students(id):
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT students.* FROM students JOIN student_classes ON students.id = student_classes.student_id JOIN classes ON classes.id = student_classes.class_id WHERE classes.id = %s;", (id,))
-    students = cursor.fetchall()
-    cursor.close()
-    return json.dumps(students, default=str)
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # Check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an empty file without a filename
-        if file.filename == '':
-            flash('No file selected, please select a file.')
-            return redirect(request.url)
-        # Check if the file is one of the allowed types/extensions
-        if not allowed_file(file.filename):
-            flash('That file extension is not allowed. Please upload a .pdf file.')
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            address = read_pdf(filename)
-            incomplete = not all(address.values())
-            if incomplete:
-                os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                flash('Please fill in all fields in the PDF.')
-                return redirect(request.url)
-            return json.dumps(address, default=str)
-
-    return render_template("upload.html")
-
-
-@app.route('/uploads/<name>')
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+    if calendar_name == None:
+        return json.dumps('Student has not given loadshedding area yet', default=str)
+    else:
+        calendar_name = calendar_name[:-4]
+    api_url = 'https://eskom-calendar-api.shuttleapp.rs/outages/' + calendar_name
+    response = requests.get(api_url)
+    return response.json()
 
 
 @app.route('/loadshedding/tshwane_campus')
 def get_tshwane_campus_loadshedding_schedule():
-    # # Get loadshedding schedule from Eskom API
-    # api_url = 'https://developer.sepush.co.za/business/2.0/area?id=tshwane-10-ninapark'
-    # response = requests.get(
-    #     api_url, headers={'token': os.environ['SEPUSHTOKEN']})
-    # loadsheddingToday = []
-    # loadsheddingTomorrow = []
-    # for event in response.json()['events']:
-    #     # Check if the date of event.start is today
-    #     if datetime.datetime.strptime(event['start'], '%Y-%m-%dT%H:%M:%S%z').date() == datetime.date.today():
-    #         loadsheddingToday.append(event)
-    #     # Check if the date of event.start is tomorrow
-    #     if datetime.datetime.strptime(event['start'], '%Y-%m-%dT%H:%M:%S%z').date() == datetime.date.today() + datetime.timedelta(days=1):
-    #         loadsheddingTomorrow.append(event)
-    # return json.dumps({'today': loadsheddingToday, 'tomorrow': loadsheddingTomorrow}, default=str)
-    return json.dumps({
-        "today": [
-            {
-                "end": "2023-06-06T18:30:00+02:00",
-                "note": "Stage 4",
-                "start": "2023-06-06T16:00:00+02:00"
-            }
-        ],
-        "tomorrow": [
-            {
-                "end": "2023-06-07T02:30:00+02:00",
-                "note": "Stage 4",
-                "start": "2023-06-07T00:00:00+02:00"
-            },
-            {
-                "end": "2023-06-07T18:30:00+02:00",
-                "note": "Stage 4",
-                "start": "2023-06-07T16:00:00+02:00"
-            }
-        ]
-    })
+    # Get loadshedding schedule from Eskom API
+    api_url = 'https://eskom-calendar-api.shuttleapp.rs/outages/gauteng-tshwane-group-10'
+    response = requests.get(api_url)
+    loadsheddingToday = []
+    loadsheddingTomorrow = []
+    for event in response.json():
+        # Check if the date of event.start is today
+        if datetime.strptime(event['start'], '%Y-%m-%dT%H:%M:%S%z').date() == datetime.today().date():
+            loadsheddingToday.append(event)
+        # Check if the date of event.start is tomorrow
+        if datetime.strptime(event['finsh'], '%Y-%m-%dT%H:%M:%S%z').date() == (datetime.today() + timedelta(days=1)).date():
+            loadsheddingTomorrow.append(event)
+    return json.dumps({'today': loadsheddingToday, 'tomorrow': loadsheddingTomorrow}, default=str)
+
+
+@app.route('/classinfo/<subject>/<classtime>/<classdate>')
+def getClassInfo(subject, classtime, classdate):
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    # Get students in class
+    api_url = os.environ['GETSTUDENTSAPI'] + subject
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        studentsInClass = response.json()
+        studentIds = [student[os.environ['STUDENTID']]
+                      for student in studentsInClass]
+    else:
+        studentsInClass = []
+        studentIds = []
+    # Get students from DB
+    cursor.execute(
+        "SELECT * FROM students WHERE student_id IN" +
+        str(tuple(studentIds)) + ";")
+    students = cursor.fetchall()
+    cursor.close()
+    # Not all students from this class are in the DB yet
+    if len(students) != len(studentsInClass):
+        foundIds = [student['student_id'] for student in students]
+        missingIds = list(set(studentIds) - set(foundIds))
+        add_missing_students_to_db(missingIds, studentsInClass)
+    # Getting distinct loadshedding areas by using a set and then converting it back to a list
+    distinctLoadsheddingAreas = set([student["calendar_name"]
+                                     for student in students if student["calendar_name"] is not None])
+    distinctLoadsheddingAreas = list(distinctLoadsheddingAreas)
+    # Get loadshedding info for each distinct area
+    loadsheddingInfo = {}
+    for loadsheddingArea in distinctLoadsheddingAreas:
+        loadsheddingArea = loadsheddingArea[:-4]
+        api_url = 'https://eskom-calendar-api.shuttleapp.rs/outages/' + \
+            loadsheddingArea
+        print('HERE:' + api_url)
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            loadsheddingInfo[loadsheddingArea] = response.json()
+    classInfo = {
+        "subject": subject,
+        "amountOfStudents": len(studentsInClass),
+        "time": classtime,
+    }
+    amountOfStudentsWithLoadshedding = 0
+    amountOfStudentsWithoutAddress = 0
+    classdate = datetime.strptime(classdate, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+    for student in students:
+        # Check if student has loadshedding
+        if student["calendar_name"] is None:
+            student["loadshedding_info"] = []
+            student["has_given_address"] = False
+            amountOfStudentsWithoutAddress += 1
+        else:
+            student["loadshedding_info"] = loadsheddingInfo[student["calendar_name"][:-4]]
+            student["has_given_address"] = True
+
+        if classtime == "AM":
+            class_start_time = time(hour=8, minute=0)
+            class_end_time = time(hour=12, minute=0)
+        elif classtime == 'PM':
+            class_start_time = time(hour=13, minute=0)
+            class_end_time = time(hour=17, minute=0)
+
+        loadshedding_periods = student["loadshedding_info"]
+        loadshedding_occurs = False
+
+        # Check if loadshedding occurs during class
+        for period in loadshedding_periods:
+            loadshedding_start_time = datetime.fromisoformat(period['start'])
+            loadshedding_end_time = datetime.fromisoformat(period['finsh'])
+
+            if loadshedding_start_time.date() <= classdate <= loadshedding_end_time.date():
+                if loadshedding_start_time.time() <= class_end_time and loadshedding_end_time.time() >= class_start_time:
+                    loadshedding_occurs = True
+                    break
+
+        if loadshedding_occurs:
+            student["has_loadshedding_during_class"] = True
+            amountOfStudentsWithLoadshedding += 1
+        else:
+            student["has_loadshedding_during_class"] = False
+
+    classInfo["students"] = students
+    classInfo["amountOfStudentsWithLoadshedding"] = amountOfStudentsWithLoadshedding
+    classInfo["amountOfStudentsWithoutAddress"] = amountOfStudentsWithoutAddress
+    classInfo["PercentageOfStudentsWithLoadshedding"] = (
+        amountOfStudentsWithLoadshedding / len(studentsInClass)) * 100
+
+    return classInfo
+
 
 # Functions
 
@@ -188,52 +188,19 @@ def get_db():
     return g.db
 
 
-@app.teardown_appcontext
+@ app.teardown_appcontext
 def close_db(error):
     if 'db' in g:
         g.db.close()
 
 
-def read_pdf(filename):
-    reader = PdfReader(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    address = reader.get_form_text_fields()
-    return address
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-def get_loadshedding_group(student):
-    # Check if student has coordinates
-    # If not, get coordinates from Nominatim API
+def add_missing_students_to_db(missingIds, studentsInClass):
     db = get_db()
     cursor = db.cursor(dictionary=True)
-    coordinates = ''
-    if student['coordinates_lat'] == None or student['coordinates_lon'] == None:
-        coordinates = get_coordinates(student['address'])
-        cursor.execute("UPDATE students SET coordinates_lat = %s, coordinates_lon = %s WHERE id = %s;",
-                       (coordinates[0], coordinates[1], student['id']))
-    else:
-        coordinates = student['coordinates_lat'], student['coordinates_lon']
-    # Get loadshedding group from Eskom API
-    api_url = 'https://developer.sepush.co.za/business/2.0/areas_nearby?lat=' + \
-        str(coordinates[0]) + '&lon=' + str(coordinates[1])
-    response = requests.get(
-        api_url, headers={'token': os.environ['SEPUSHTOKEN']})
-    loadshedding_group = response.json()['areas'][0]['id']
-    # Save loadshedding group to database
-    cursor.execute("UPDATE students SET loadsheddingGroup = %s WHERE id = %s;",
-                   (loadshedding_group, student['id']))
+    for studentId in missingIds:
+        student = [
+            student for student in studentsInClass if student[os.environ['STUDENTID']] == studentId][0]
+        cursor.execute("INSERT INTO students (student_id, name, surname) VALUES (%s, %s, %s);",
+                       (student[os.environ['STUDENTID']], student['Name'], student['Surname']))
     cursor.close()
-    return loadshedding_group
-
-
-def get_coordinates(address):
-    # Get coordinates from Nominatim API
-    api_url = 'https://nominatim.openstreetmap.org/search?q=' + address + '&format=json'
-    response = requests.get(api_url)
-    coordinates = response.json()[0]['lat'], response.json()[0]['lon']
-    print(coordinates)
-    return coordinates
+    return
